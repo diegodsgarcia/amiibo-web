9ae4d26640cfb81b8cc966175236703a82b00da6
diegodsgarcia <diego.ds.garcia@gmail.com>
2020-04-13 12:15:28 -0300

    add react project

---
+++ react/README.md
@@ -0,0 +1,5 @@
+# Amiibo React
+
+Você pode rodar este projeto com o comando:
+
+### `yarn start`
---
+++ react/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+const url = 'https://www.amiiboapi.com/api/amiibo'
+
+function App() {
+  const [amiibos, setAmiibos] = useState([])
+
+  useEffect(() => {
+    fetch(url).then(result => result.json()).then(({ amiibo }) => setAmiibos(amiibo))
+  }, [])
+
+  return (
+    <main>
+      <h1 class="title">Amiibos</h1>
+      <ul class="list">
+        {amiibos.map(({ name, image, amiiboSeries, release }) => (
+          <li key={name} className="item">
+            <img src={image} alt={name} title={name} />
+            <div class="item-description">
+              <h2 class="item-name">{name}</h2>
+              <p>Game Serie: {amiiboSeries}</p>
+              <p>Release: {release.eu ?? release.jp ?? release.na ?? release.au}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export default App;
